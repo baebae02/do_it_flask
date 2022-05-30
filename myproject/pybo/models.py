bef52f00f521eb4361944bc8cd777c269b13401c
Seo Hyeon Bae <bae4614@gmail.com>
2022-05-30 12:56:07 +0900

    feat: auth service

--- myproject/pybo/models.py
+++ myproject/pybo/models.py
@@ -1,10 +1,12 @@
 from pybo import db
 
+
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subject = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+
 
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,3 +14,10 @@ class Answer(db.Model):
     question = db.relationship('Question', backref=db.backref('answer_set'))
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
