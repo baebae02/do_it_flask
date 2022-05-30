8e6455978438dbd064a70e83ee894974029f3973
Seo Hyeon Bae <bae4614@gmail.com>
2022-05-31 08:36:59 +0900

    feat: login & logout

--- myproject/pybo/views/auth_views.py
+++ myproject/pybo/views/auth_views.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, request, redirect, url_for, flash, render_template
-from werkzeug.security import generate_password_hash
+from flask import Blueprint, request, redirect, url_for, flash, render_template, session, g
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from pybo import db
-from pybo.forms import UserCreateForm
+from pybo.forms import UserCreateForm, UserLoginForm
 from pybo.models import User
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
@@ -25,3 +25,34 @@ def signup():
     return render_template('auth/signup.html', form=form)
 
 
+@bp.route('/login/', methods=('GET', 'POST'))
+def login():
+    form = UserLoginForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        error = None
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user:
+            error = "존재하지 않는 사용자입니다."
+        elif not check_password_hash(user.password, form.password.data):
+            error = "비밀번호가 올바르지 않습니다."
+        if error is None:
+            session.clear()
+            session['user_id'] = user.id
+            return redirect(url_for('main.index'))
+        flash(error)
+    return render_template('auth/login.html', form=form)
+
+
+@bp.route('/logout/')
+def logout():
+    session.clear()
+    return redirect(url_for('main.index'))
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get(user_id)
