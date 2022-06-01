f345cc7c17b534f33a86a433c5f8b79ff5cd74d8
bread4614 <bread4614@gmail.com>
2022-06-01 13:53:06 +0900

    feat: add logging

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for, render_template
+from flask import Blueprint, url_for, render_template, current_app
 from werkzeug.utils import redirect
 from pybo.models import Question
 
@@ -12,4 +12,5 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
+    current_app.logger.info("INFO 레벨로 출력")
     return redirect(url_for('question._list'))
