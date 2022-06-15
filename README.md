f7c79ff721dbefb7fe03dee48aa718477afdaf15
bread4614 <bread4614@gmail.com>
2022-06-15 21:26:37 +0900

    feat: nullable False

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # do_it_flask
 flask study with do_it_flask
+**서버 재부팅**
+sudo systemctl restart myproject.service
--- pybo/models.py
+++ pybo/models.py
@@ -39,9 +39,9 @@ class Answer(db.Model):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
-    nickname = db.Column(db.String(150), nullable=True, default='00')
+    nickname = db.Column(db.String(150), nullable=False)
     password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     baekjoon = db.Column(db.String(120), unique=True, nullable=True)
-    phone = db.Column(db.String(20), nullable=True, default='01026487195')
+    phone = db.Column(db.String(20), unique=True, nullable=False)
 
